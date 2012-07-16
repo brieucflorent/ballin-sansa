d6d3a9b0fc5f645b41c07b3ebc6a125dbc0b2826
brieuc <brieucflorent@gmail.com>
2012-07-16 11:36:46 -0700

    ordering year

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,22 +15,22 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
 
     if params[:weekday] == 'now'
-      @movies = Movie.where("rating >=? and cweek = ?",id,Date.today.cweek).order("release_date DESC, title ASC")
+      @movies = Movie.where("rating >=? and cweek = ?",id,Date.today.cweek).order("year DESC,release_date DESC, title ASC")
     elsif params[:weekday] =~ /\d+/
-      @movies = Movie.where("rating >=? and cweek = ?",id,params[:weekday]).order("release_date DESC, title ASC")
+      @movies = Movie.where("rating >=? and cweek = ?",id,params[:weekday]).order("year DESC,release_date DESC, title ASC")
     else
-      @movies = Movie.where("rating >=?",id).order("release_date DESC, title ASC")
+      @movies = Movie.where("rating >=?",id).order("year DESC,release_date DESC, title ASC")
     end
 
   end
 
   def index
     if params[:weekday] == 'now'
-      @movies = Movie.where("cweek = ?",Date.today.cweek).order("release_date DESC, title ASC")
+      @movies = Movie.where("cweek = ?",Date.today.cweek).order("year DESC,release_date DESC, title ASC")
     elsif params[:weekday] =~ /\d+/
-      @movies = Movie.where("cweek = ?",params[:weekday]).order("release_date DESC, title ASC")
+      @movies = Movie.where("cweek = ?",params[:weekday]).order("year DESC,release_date DESC, title ASC")
     else
-      @movies = Movie.find(:all,:order => "release_date DESC, title ASC")
+      @movies = Movie.find(:all,:order => "year DESC,release_date DESC, title ASC")
     end
   end
 
